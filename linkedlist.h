3d4e0478a5bdf7467f8b53cfdb768f6ce45fd1bc
u-lo-l <jeffk0408@naver.com>
2022-01-23 22:03:51 +0900

    linkedlist init

---
+++ linkedlist.h
@@ -0,0 +1,32 @@
+#ifndef _LINKEDLIST_
+#define _LINKEDLIST_
+
+typedef struct ListNodeType
+{
+	int data;
+	struct ListNodeType* pLink;
+} ListNode;
+
+typedef struct LinkedListType
+{
+	int currentElementCount;	// 현재 저장된 원소의 개수
+	ListNode headerNode;		// 헤더 노드(Header Node)
+} LinkedList;
+
+LinkedList* createLinkedList();
+int addLLElement(LinkedList* pList, int position, ListNode element);
+int removeLLElement(LinkedList* pList, int position);
+ListNode* getLLElement(LinkedList* pList, int position);
+
+void clearLinkedList(LinkedList* pList);
+int getLinkedListLength(LinkedList* pList);
+void deleteLinkedList(LinkedList* pList);
+#endif
+
+#ifndef _COMMON_LIST_DEF_
+#define _COMMON_LIST_DEF_
+
+#define TRUE		1
+#define FALSE		0
+
+#endif
