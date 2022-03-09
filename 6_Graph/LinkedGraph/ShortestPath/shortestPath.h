dd9a90f2419c19882b1983f9b49659ce454f50e6
u-lo-l <jeffk0408@naver.com>
2022-03-09 13:20:13 +0900

    init shortest path header

--- 6_Graph/LinkedGraph/ShortestPath/shortestPath.h
+++ 6_Graph/LinkedGraph/ShortestPath/shortestPath.h
@@ -1,8 +1,16 @@
 #ifndef SHORTESTPATH_H
 # define SHORTESTPATH_H
+# define INF 2147483647
+# include "../linkedgraph.h"
 
 /*Dijkstra*/
 
+int		*dijkstra(LinkedGraph *pGraph, int startVertex);
+
 /*Floyd*/
+
+int		**initDistArray(LinkedGraph *pGraph);
+
+void	floyd(int **distArray, LinkedGraph);
 
 #endif
